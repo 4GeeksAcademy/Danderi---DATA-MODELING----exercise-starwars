b6341da0eaedf8760630d861f40dcfb4d409d33f
Daniel Anderi <100786824+danderi@users.noreply.github.com>
2024-04-24 00:38:31 +0000

    Fixes the erros by adding the s to planets and characters

--- src/models.py
+++ src/models.py
@@ -12,23 +12,11 @@ class User(Base):
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-    email = Column(String(80), nullable=False)
-    password = Column(String(100))
-    subscription_date = Column(String(10))
+    name = Column(String(180), nullable=False)
+    email = Column(String(180), nullable=False)
+    password = Column(String(180), nullable=False)
+    subscription_date = Column(String(180))
 
-class Favorite(Base):
-    __tablename__ = 'favorite'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    # planet_favorite = Column(Integer, ForeignKey("planet.id"))
-    # planet = relationship(Planets)
-    # character_favorite = Column(Integer, ForeignKey("character.id"))
-    # character = relationship(Characters)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
-    
     
 class Planets(Base):
     __tablename__ = "planets"
@@ -37,8 +25,8 @@ class Planets(Base):
     population = Column (Integer)
     diameter = Column (Integer)
     gravity = Column (Integer)
-    favorite_id = Column(Integer, ForeignKey('favorite.id'))
-    favorite = relationship(Favorite)
+    # favorite_id = Column(Integer, ForeignKey('favorite.id'))
+    # favorite = relationship(Favorite)
 
     
 class Characters(Base):
@@ -46,9 +34,22 @@ class Characters(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     height = Column (Integer)
-    hair_color = Column (String)
-    character_favorite = Column(Integer, ForeignKey("favorite.id"))
-    character = relationship(Favorite)
+    hair_color = Column(String(180))
+    # character_favorite = Column(Integer, ForeignKey("favorite.id"))
+    # character = relationship(Favorite)
+
+
+class Favorite(Base):
+    __tablename__ = 'favorite'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    planet_id = Column(Integer, ForeignKey('planets.id'))
+    planet = relationship(Planets)
+    character_id = Column(Integer, ForeignKey('characters.id'))
+    character = relationship(Characters)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
     def to_dict(self):
         return {}
